a17ba0405188d4de0548528a06a5caa0637562f6
minkyngkm <min.kim@canonical.com>
2022-06-20 13:06:16 +0200

    Apply suggetion after second review

--- static/js/tabs.js
+++ static/js/tabs.js
@@ -38,9 +38,11 @@ function setupTabs() {
       }
     }
 
-    tab.setAttribute("aria-selected", "true");
-    tab.classList.toggle("active");
-    tabContent.classList.toggle("u-hide");
+    if (tab.getAttribute("aria-selected") !== "true") {
+      tab.setAttribute("aria-selected", "true");
+      tab.classList.toggle("active");
+      tabContent.classList.toggle("u-hide");
+    }
 
     if (tab.classList.contains("active")) {
       var breakpoint = 620;
